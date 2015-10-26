1d9a9a52237889cec9577d7d9a711248597d7e27
Ethan L <3laverack@gmail.com>
2015-10-25 22:41:43 -0400

    just creating some test code

---
+++ gamepad_interface.py
@@ -0,0 +1,17 @@
+# GAMEPAD INTERFACE
+# Ethan Laverack
+# Description: Uses PyGame library to pull data off of gamepad and send axis
+#              and button states to ui_hub through standard out
+
+# get the pygame library
+import pygame
+
+# initialize pygame
+pygame.init()
+
+#initalizes joystick module
+pygame.joystick.init()
+
+#
+joysticks = pygame.joystick.get_count()
+#print str(joysticks) + " joystick(s) detected!"
