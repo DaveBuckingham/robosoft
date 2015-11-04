04b3c87f19f066501af99de41558f2e6784e47d7
jmusca <jmusca@cs.tufts.edu>
2015-11-04 16:26:30 -0500

    Added global variables for button/axes

--- global_data.py
+++ global_data.py
@@ -12,3 +12,23 @@ analog_0_confirmed  = 0;
 analog_1_confirmed  = 0;
 digital_0_confirmed = False;
 digital_1_confirmed = False;
+
+
+
+-# (I'M JUST MAKING THIS UP, WE'LL HAVE TO CHANGE IT! -db)
+-# BUTTON NAMES
+BUTTON_X = 'button_2'
+BUTTON_Y = 'button_3'
+BUTTON_A = 'button_0'
+BUTTON_B = 'button_1'
+
+BUTTON_SELECT = 'button_6'
+BUTTON_START = 'button_7'
+BUTTON_LEFT_STICK = 'button_8'
+BUTTON_RIGHT_STICK = 'button_9'
+
+AXIS_X_LEFT_STICK   = 'axis_0'
+AXIS_Y_LEFT_STICK   = 'axis_1'
+AXIS_TRIGGERS = 'axis_2"
+AXIS_X_RIGHT_STICK   = 'axis_3'
+AXIS_Y_RIGHT_STICK   = 'axis_4'
