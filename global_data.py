7b9b7d03cfab896c8b4e5d57302a3ac3dcd2e5dd
jmusca <jmusca@cs.tufts.edu>
2015-11-04 16:00:55 -0500

    removed the last third (translation)

--- global_data.py
+++ global_data.py
@@ -12,13 +12,3 @@ analog_0_confirmed  = 0;
 analog_1_confirmed  = 0;
 digital_0_confirmed = False;
 digital_1_confirmed = False;
-
-# (I'M JUST MAKING THIS UP, WE'LL HAVE TO CHANGE IT! -db)
-# BUTTON NAMES
-BUTTON_X = 0
-BUTTON_Y = 1
-BUTTON_A = 2
-BUTTON_B = 3
-AXIS_0   = 0
-AXIS_1   = 1
-SELECT = 6
